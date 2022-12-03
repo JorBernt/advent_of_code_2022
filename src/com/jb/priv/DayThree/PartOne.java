5a511ff621bd6a800605c59853a449450b38d7ad
JorBernt <berntsen.jorgen@gmail.com>
2022-12-03 11:33:36 +0100

    Day 3 done

---
+++ src/com/jb/priv/DayThree/PartOne.java
@@ -0,0 +1,30 @@
+package com.jb.priv.DayThree;
+
+import com.jb.priv.InputReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartOne {
+    public static void main(String[] args) {
+        var input = InputReader.getInput("src/com/jb/priv/DayThree/input.txt");
+        List<Character> items = new ArrayList<>();
+        outer:
+        for (var s : input) {
+            var compA = s.substring(0, s.length() / 2);
+            var compB = s.substring(s.length() / 2);
+            for (char c : compA.toCharArray()) {
+                for (char d : compB.toCharArray()) {
+                    if (c == d) {
+                        items.add(c);
+                        continue outer;
+                    }
+                }
+            }
+        }
+        long sum = items.stream()
+                .mapToInt(a -> Character.isUpperCase(a) ? a - 'A' + 27 : a - 'a' + 1)
+                .sum();
+        System.out.println(sum);
+    }
+}
---
+++ src/com/jb/priv/DayThree/PartTwo.java
@@ -0,0 +1,36 @@
+package com.jb.priv.DayThree;
+
+import com.jb.priv.InputReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartTwo {
+    public static void main(String[] args) {
+        var input = InputReader.getInput("src/com/jb/priv/DayThree/input.txt");
+        int count = 1;
+        List<Character> badges = new ArrayList<>();
+        for (int i = 0; i < input.size(); i += 3) {
+            char badge = getBadge(input, i);
+            badges.add(badge);
+        }
+        long sum = badges.stream()
+                .mapToInt(a -> Character.isUpperCase(a) ? a - 'A' + 27 : a - 'a' + 1)
+                .sum();
+        System.out.println(sum);
+    }
+
+    static char getBadge(List<String> input, int from) {
+        for (char a : input.get(from).toCharArray()) {
+            for(char b : input.get(from + 1).toCharArray()) {
+                if(a == b) {
+                    for(char c : input.get(from + 2).toCharArray()) {
+                        if(b == c)
+                            return c;
+                    }
+                }
+            }
+        }
+        return '-';
+    }
+}
